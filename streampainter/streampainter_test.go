341fc4b2a4115b1cb492c750d2ec2ad08bd5e539
Raul Alvarado <raul.alvarado@appliedis.com>
2018-08-17 14:49:01 -0400

    added better error messages in streampainter test

--- streampainter/streampainter_test.go
+++ streampainter/streampainter_test.go
@@ -3,27 +3,16 @@ package streampainter
 import (
 	"testing"
 
-<<<<<<< HEAD
 	"github.com/fatih/color"
-=======
-	"github.com/faith/color"
->>>>>>> 9060b75... added red text for stderr output
 )
 
 func TestStreamPainter(t *testing.T) {
 	writer := NewStreamPainter(color.FgRed)
 	_, err := writer.Write([]byte("should be in red"))
 	if err != nil {
-<<<<<<< HEAD
 		t.Errorf("Error should be 'nil', but was %v", err.Error())
 	}
 	if writer.paintColor != color.FgRed {
 		t.Errorf("Assigned color expected FgRed, but was %v", writer.paintColor)
-=======
-		t.Error("ERRORED")
-	}
-	if writer.paintColor != color.FgRed {
-		t.Error("ERRORED")
->>>>>>> 9060b75... added red text for stderr output
 	}
 }
